e0b9abdcea315d56c0e77bd3201cd6c2f7c8d520
Rajko Albrecht <ral@alwins-world.de>
2016-02-20 09:50:51 +0100

    Don't access settings / load keys on rake tasks

--- config/initializers/initkeys.rb
+++ config/initializers/initkeys.rb
@@ -1,11 +1,13 @@
-key_pem = File.read Setting.ssl_private_key_file
-$ssl_key = OpenSSL::PKey::RSA.new key_pem
+if ActiveRecord::Base.connection.tables.include?('settings') and !defined?(::Rake)
+  key_pem = File.read Setting.ssl_private_key_file
+  $ssl_key = OpenSSL::PKey::RSA.new key_pem
 
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    if forked
-      key_pem = File.read Setting.ssl_private_key_file
-      $ssl_key = OpenSSL::PKey::RSA.new key_pem
+  if defined?(PhusionPassenger)
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+      if forked
+        key_pem = File.read Setting.ssl_private_key_file
+        $ssl_key = OpenSSL::PKey::RSA.new key_pem
+      end
     end
   end
 end
