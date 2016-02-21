3803782a5904f86c6c6c72b245311c52e82baa4e
Rajko Albrecht <ral@alwins-world.de>
2016-02-22 00:18:53 +0100

    removed logging yml configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.show_log_configuration = false
+
 end
