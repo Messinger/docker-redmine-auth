c18e131b211a19ddd9555e265b9bbe1eadf7f136
Rajko Albrecht <ral@alwins-world.de>
2016-02-22 22:07:24 +0100

    Use unicorn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   if ENV.key? 'PRODUCTION_TO_STDOUT'
     config.log_to=['stdout']
-    config.log_level = :debug
+    config.log_level = :info
   else
     config.log_to=['file']
   end
