0a6d1eac8372f1a1eefde385146d0c7dcaaea997
Rajko Albrecht <rajko.albrecht@7p-group.com>
2016-02-23 09:41:59 +0100

    add listener to unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,9 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
+listen 3000
+listen '[::]:3000', :ipv6only => true
+
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
