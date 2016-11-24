391794775a7dca829913d6810cc857bf634c91c9
Rajko Albrecht <ral@alwins-world.de>
2016-11-24 10:37:34 +0100

    catch-all route which will load angular page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
 
   match '/auth_mapper', :to => 'registry_admin#auth_mapper', :via => [:post]
 
+  match '*a', :to => 'registry_admin#index', :via => [:get,:post,:put,:options,:delete,:head]
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
