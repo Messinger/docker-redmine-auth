2bea3dc5842c9f05ece44fb0aa923068f938c84c
Rajko Albrecht <ral@alwins-world.de>
2016-12-03 02:48:14 +0100

    re-add theme - was not the reason for the problems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 
 //= require jquery/dist/jquery
 //= require bootstrap-sass-official/assets/javascripts/bootstrap
+//= require sb-admin-2
+//= require metisMenu
 //= require angular/angular
 //= require angular-bootstrap/ui-bootstrap
 //= require angular-bootstrap/ui-bootstrap-tpls
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module DockerRedmineAuth
 
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.paths << Rails.root.join("vendor","assets","fonts")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
   end
